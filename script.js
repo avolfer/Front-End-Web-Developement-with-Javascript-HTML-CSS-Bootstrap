cf295107a8d89933c44bf90cd13ee18f2bd10e6b
avolfer <jakubstuba2004@gmail.com>
2023-03-06 13:27:18 +0100

    if else statements in script

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-alert("test");
-let FirstName = "Jakub";
-let LastName = "Stuba";
+let userAge = 24;
 
-console.log(FirstName + " " + LastName);
+if (userAge < 18) {
+    console.log("he/she can not enter the pub");
+} else if (userAge > 18 && userAge < 23) {
+    console.log("he/she can enter but can not drink alcohol");
+} else {
+    console.log("he/she can enter and drink %")
+}
