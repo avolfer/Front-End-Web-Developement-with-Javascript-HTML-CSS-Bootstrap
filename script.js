93a732476f041329582cc53302ff4ab970368a78
avolfer <jakubstuba2004@gmail.com>
2023-03-07 10:40:32 +0100

    looping

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let userAge = 24;
+// let userAge = 24;
 
-if (userAge < 18) {
-    console.log("he/she can not enter the pub");
-} else if (userAge > 18 && userAge < 23) {
-    console.log("he/she can enter but can not drink alcohol");
-} else {
-    console.log("he/she can enter and drink %")
-}
+// if (userAge < 18) {
+//     console.log("he/she can not enter the pub");
+// } else if (userAge > 18 && userAge < 23) {
+//     console.log("he/she can enter but can not drink alcohol");
+// } else {
+//     console.log("he/she can enter and drink %")
+// }
+
+// let day;
+// day = "sunday"
+
+// switch (day) {
+//     case "saturday":
+//         console.log("today is saturday");
+//         break;
+//     case "monday":
+//         console.log("today is monday");
+//         break;
+//     default:
+//         console.log("I don't know the day")
+// }
+
+
