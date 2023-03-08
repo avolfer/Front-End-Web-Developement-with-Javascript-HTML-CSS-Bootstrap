ddb195c6ee8c624aa45f35dcb47a55691894fe72
avolfer <jakubstuba2004@gmail.com>
2023-03-08 13:04:13 +0100

    .

--- script.js
+++ script.js
@@ -69,18 +69,20 @@
 // }
 
 
-for (let i = 1; i <= 10; i++) {
-    if (i % 2 !== 0) {
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 !== 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
 
 
 
-for (let i = 1; i <= 10; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-        break;
-    }
-}
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//         break;
+//     }
+// }
+
+
