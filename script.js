699e4bf9269ab4cba13385aefbfdb6d4bc19d21d
avolfer <jakubstuba2004@gmail.com>
2023-03-08 09:49:46 +0100

    looping break

--- script.js
+++ script.js
@@ -1,25 +1,86 @@
 // let userAge = 24;
 
 // if (userAge < 18) {
-//     console.log("he/she can not enter the pub");
+//   console.log("he/she can not enter the pub");
 // } else if (userAge > 18 && userAge < 23) {
-//     console.log("he/she can enter but can not drink alcohol");
+//   console.log("he/she can enter but can not drink alcohol");
 // } else {
-//     console.log("he/she can enter and drink %")
+//   console.log("he/she can enter and drink %");
 // }
 
 // let day;
-// day = "sunday"
+// day = "sunday";
 
 // switch (day) {
-//     case "saturday":
-//         console.log("today is saturday");
-//         break;
-//     case "monday":
-//         console.log("today is monday");
-//         break;
-//     default:
-//         console.log("I don't know the day")
+//   case "saturday":
+//     console.log("today is saturday");
+//     break;
+//   case "monday":
+//     console.log("today is monday");
+//     break;
+//   default:
+//     console.log("I don't know the day");
 // }
 
 
+// let number = 1;
+
+// while (number < 11) {
+//     console.log(number);
+//     number+=2;
+// }
+
+
+// let number = 132;
+
+// while (number < 149) {
+//     console.log(number);
+//     number += 1;
+// }
+
+// let count = 25;
+
+// while (count <= 100) {
+//     if (count % 7 === 0) {
+//         console.log(count);
+//     }
+//     count++;
+// }
+
+
+// for (let i = 1; i < 11; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 132; i < 149; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 132; i <= 148; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(i);
+//     }
+// }
+
+// for (let i = 25; i <= 100; i++){
+//     if ((i % 7 === 0) && (i % 5 === 0)) {
+//         console.log(i);
+//     }
+// }
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 !== 0) {
+        continue;
+    }
+    console.log(i);
+}
+
+
+
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+        break;
+    }
+}
